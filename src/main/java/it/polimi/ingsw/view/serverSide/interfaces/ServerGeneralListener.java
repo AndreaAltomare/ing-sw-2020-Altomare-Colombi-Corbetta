6834bc155df3b304d870da56e2ac55a84d35ba4c
AndreaAltomare <andrea1.altomare@mail.polimi.it>
2020-04-24 14:23:32 +0200

    Minor fix

--- src/main/java/it/polimi/ingsw/view/serverSide/interfaces/ServerGeneralListener.java
+++ src/main/java/it/polimi/ingsw/view/serverSide/interfaces/ServerGeneralListener.java
@@ -10,6 +10,7 @@ import it.polimi.ingsw.view.serverSide.ClientStatus;
  * @author Giorgio Corbetta
  */
 public interface ServerGeneralListener {
-    public void onStatusChange(ClientStatus applicationStatus);
+    public void onStatusChange(ClientStatus clientStatus);
+    public void onNextStatus();
     public void serverSendData(); // Game match general data (to respond to viewRequestData, receive data from Server)
 }
