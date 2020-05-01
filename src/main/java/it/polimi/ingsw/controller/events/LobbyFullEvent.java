d436f380a7de11d3728ed8d26db91cbd9a3f2008
AndreaAltomare <andrea1.altomare@mail.polimi.it>
2020-05-01 23:49:07 +0200

    Added MVEvent to notify when the game lobby is full.

---
+++ src/main/java/it/polimi/ingsw/controller/events/LobbyFullEvent.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.controller.events;
+
+import java.util.EventObject;
+
+/**
+ * Event: Lobby is full. No other players are allowed in.
+ * [MVEvent]
+ */
+public class LobbyFullEvent extends EventObject {
+    private final String message;
+
+    public LobbyFullEvent() {
+        super(new Object());
+        this.message = "Lobby is full! No other players are allowed in until a new game starts.";
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+    }
+}
