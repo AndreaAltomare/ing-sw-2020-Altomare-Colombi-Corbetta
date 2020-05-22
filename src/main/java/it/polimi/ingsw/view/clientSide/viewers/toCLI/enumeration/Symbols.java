7628ee878da73fa5eca7f80151af6f6c90cd626d
Marco Colombi <marco3.colombi@mail.polimi.it>
2020-05-22 22:34:45 +0200

    New CLI status class and Symbol classes for board and gods

---
+++ src/main/java/it/polimi/ingsw/view/clientSide/viewers/toCLI/enumeration/Symbols.java
@@ -0,0 +1,66 @@
+package it.polimi.ingsw.view.clientSide.viewers.toCLI.enumeration;
+
+public enum Symbols {
+
+    WORKER_1(   " O ",
+                "/|\\",
+                "/ \\"),
+
+    WORKER_2(   "\\O ",
+                " |\\",
+                "/ \\"),
+
+    WORKER_3(   "\\O/ ",
+                " | ",
+                "/ \\"),
+
+    DOME(       "     ",
+                " ___ ",
+                "/___\\"),
+
+    BLOCK(      "    ",
+                " __ ",
+                "|__|"),
+
+    SELECTION_LEFT( "   /",
+                    ">>( ",
+                    "   \\"),
+
+    SELECTION_RIGHT("\\   ",
+                    " )<<",
+                    "/   ");
+
+    private String upRepresentation;
+    private String middleRepresentation;
+    private String downRepresentation;
+
+    Symbols(String upRepresentation, String middleRepresentation, String downRepresentation) {
+        this.upRepresentation = upRepresentation;
+        this.middleRepresentation = middleRepresentation;
+        this.downRepresentation = downRepresentation;
+    }
+
+    /**
+     * Returns the parameter called upRepresentation of GodSymbol chosen
+     * @return upRepresentation parameter
+     */
+    public String getUpRepresentation() {
+        return upRepresentation;
+    }
+
+    /**
+     * Returns the parameter called middleRepresentation of GodSymbol chosen
+     * @return middleRepresentation parameter
+     */
+    public String getMiddleRepresentation() {
+        return middleRepresentation;
+    }
+
+    /**
+     * Returns the parameter called downRepresentation of GodSymbol chosen
+     * @return downRepresentation parameter
+     */
+    public String getDownRepresentation() {
+        return downRepresentation;
+    }
+}
