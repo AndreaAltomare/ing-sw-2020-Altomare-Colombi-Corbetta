51b6d71c549122473ce2d104c924fa66a4c53455
GioCor <giorgiocor98@gmail.com>
2020-06-24 10:45:08 +0200

    added credits

--- src/main/java/it/polimi/ingsw/view/clientSide/viewers/toGUI/statusClasses/GUIGameOverStatus.java
+++ src/main/java/it/polimi/ingsw/view/clientSide/viewers/toGUI/statusClasses/GUIGameOverStatus.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.view.clientSide.viewers.toGUI.statusClasses;
 
 import it.polimi.ingsw.view.clientSide.viewers.interfaces.StatusViewer;
 import it.polimi.ingsw.view.clientSide.viewers.interfaces.Viewer;
+import it.polimi.ingsw.view.clientSide.viewers.toGUI.helperPanels.elements.PanelComponent;
 import it.polimi.ingsw.view.clientSide.viewers.toGUI.helperPanels.elements.PanelImageButton;
 import it.polimi.ingsw.view.clientSide.viewers.toGUI.helperPanels.skeleton.TitlePanel;
 import it.polimi.ingsw.view.clientSide.viewers.toGUI.helperPanels.utilities.BackgroundPanel;
@@ -33,6 +34,13 @@ public class GUIGameOverStatus extends GUIStatusViewer {
         });
 
         panel.add(new PanelImageButton(0.6, 0.15, 0.2, 0.8 , quitter, "/img/trappings/redButton.png", "quit"));
+
+        JLabel credits = new JLabel("<html> sounds from: <a href=\"www.Zapslat.com\"> Zapslat.com </a> </html>");
+        //label.setText("<html>" +  +"</html>");
+        JPanel textPanel = new PanelComponent(0.9, 0.1, 0.4, 0.7, credits);
+        textPanel.setOpaque(false);
+        panel.add(textPanel);
+
         return panel;
     }
 
